c8bc2d66e19c0405620afbf4fdc4f189c6f6ea2d
nmo20 <nmo212182@gmail.com>
2025-09-11 00:07:17 +0530

    Dynamic Custom ListView

--- app/src/main/java/com/example/android3/MainActivity.kt
+++ app/src/main/java/com/example/android3/MainActivity.kt
@@ -1,37 +1,76 @@
 package com.example.android3
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.ListView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel: MyViewModel by viewModels()
+    lateinit var et1: EditText
+    lateinit var et2:EditText
+    lateinit var add: Button
+    private lateinit var imageView: ImageView
+    private var selectedImageUri: Uri? = null
+
+    private val PICK_IMAGE = 1
+
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val btn= findViewById<Button>(R.id.btn)
-        val txtView=findViewById<TextView>(R.id.txtView)
+        var listView=findViewById<ListView>(R.id.listView)
 
-        viewModel.result.observe(this, Observer {
-            txtView.text = it
-        })
+        var list= mutableListOf<MyItem>()
+        et1=findViewById(R.id.et1)
+        et2=findViewById(R.id.et2)
+        add=findViewById(R.id.add)
+        imageView = findViewById(R.id.im2)
 
-        btn.setOnClickListener {
-           viewModel.startWork()
+        var ad= MyAdapterForCustomListView(
+            this, R.layout.custom_list_view, list)
+        listView.adapter=ad
+
+        imageView.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            )
+            startActivityForResult(intent, PICK_IMAGE)
+        }
+
+        add.setOnClickListener {
+            selectedImageUri?.let { list.add(MyItem(et1.text.toString(),et2.text.toString(), it))
+                Toast.makeText(this, "Added", Toast.LENGTH_SHORT).show()
+                ad.notifyDataSetChanged()
+            } ?: run {
+                Toast.makeText(this, "Please select an image first",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            selectedImageUri = data?.data
+            selectedImageUri?.let {
+                val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver
+                    , it)
+                imageView.setImageBitmap(bitmap)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/android3/MyItem.kt
@@ -0,0 +1,6 @@
+package com.example.android3
+
+import android.net.Uri
+
+class MyItem (val title: String, val subTitle: String, val img: Uri) {
+}
